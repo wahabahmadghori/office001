f9daf03d36b685053d32ab4a5669ab6d668933f5
Wahab Ahmad Ghori <wahab.ahmad@DIGISKILLS.PK>
2023-01-13 10:00:37 +0500

    mongodb connection fixed

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const express  = require('express')
 const serverless = require('serverless-http')
 const app = express()
 //const bodyParser = require('body-parser')
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 //const categoryRouter = require('./routers/categories');
 //const poetryRouter = require('./routers/poetries');
@@ -32,6 +32,16 @@ app.listen(3000, ()=>{
     });
     
 })*/
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName:'urdu_shayari'
+}).then(()=>{
+    console.log('Database Connection Established and Server is running at http://localhost:3000/api/v1')
+}).catch(e=>{
+    console.log(e)
+});
 
 const router = express.Router()
 
