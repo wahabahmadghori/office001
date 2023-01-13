2f2b5a37994ef25bce9e350fa79502bf705d2171
Wahab Ahmad Ghori <wahab.ahmad@DIGISKILLS.PK>
2023-01-13 10:20:23 +0500

    mongodb connection fixed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 const express  = require('express')
 const serverless = require('serverless-http')
-const mongoose = require('mongoose');
 const app = express()
 //const bodyParser = require('body-parser')
 //const mongoose = require('mongoose');
